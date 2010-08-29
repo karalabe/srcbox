ccde7634ef67194a53ff8559f75c8ae17163734e
Péter Szilágyi <peter@karalabe.com>
2010-08-29 17:58:12 +0300

    Ported GitBox to Fedora.
    Implemented automatic git installation in Fedora using yum with
    either sudo or su (depending on the privilege level of the user).

--- setup/git/linux.sh
+++ setup/git/linux.sh
@@ -20,14 +20,35 @@ if [ -f /etc/SuSE-release ]; then
 elif [ -f /etc/lsb-release ]; then
     dist=`cat /etc/lsb-release | grep _ID | tr -d "\n" | sed s/.*=//`
     ver=`cat /etc/lsb-release | grep _REL | tr -d "\n" | sed s/.*=//`
-    echo "$dist $ver"
     if [ "$dist" == 'Ubuntu' ]; then
         echo "Ubuntu v$ver found."
         echo "Installing git with sudo apt-get..."
         sudo apt-get --assume-yes install git-core
         exit
     fi
-#Unknown distribution    
+# Fedora distribution
+elif [ -f /etc/fedora-release ]; then
+    dist=`cat /etc/fedora-release | tr "\n" ' ' | sed s/\ release.*//`
+    ver=`cat /etc/fedora-release | tr "\n" ' ' | sed s/.*release\ // | sed s/\ .*//`
+    if [ `echo $dist | cut -d ' ' -f 1` == 'Fedora' ]; then
+        echo "Fedora v$ver found."
+        echo "Checking for sudo access to yum..."
+        sudo -l yum
+        if [ $? -eq 0 ]; then
+            echo "Installing git with sudo yum..."
+            sudo yum --assumeyes install git-core
+            exit
+        else
+            echo "Installing git with root privileges..."
+            su root -c "yum --assumeyes install git-core"
+            exit
+        fi
+    else
+        echo "Unsupported Fedora distribution."
+        echo "Please install git manually."
+        exit 1
+    fi
+# Unknown distribution    
 else
     echo "Unsupported Linux distribution."
     echo "Please install git manually."
